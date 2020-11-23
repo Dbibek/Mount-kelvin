cd1268c9323a69693d87f4b55d65370910ec212f
Bibek Dhakal <47821826+Dbibek@users.noreply.github.com>
2020-11-23 17:24:13 +0200

    commented line  deleted

--- src/config.js
+++ src/config.js
@@ -2,7 +2,6 @@ const API_KEY = process.env.REACT_APP_API_KEY;
 const API_URL = process.env.REACT_APP_API_URL;
 
 const config= {
-  API_URL_WITH_KEY: `${API_URL}/${API_KEY}`,
-  //API_URL_WITH_KEY : `https://api.mountkelvin.com/v1/site/needed-endurable-plough`,
+  API_URL_WITH_KEY: `${API_URL}/${API_KEY}`
 };
 export default config
